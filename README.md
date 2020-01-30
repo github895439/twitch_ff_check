959f3e6405cf382aac5a74fe5728786d2a9fdef4
KEY <gl176705@gmail.com>
2020-01-30 22:04:39 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# twitch_ff_check
+本内容を利用した場合の一切の責任を私は負いません。
+
+# 環境
+- OS  
+OS 名:                  Microsoft Windows 10 Home
+OS バージョン:          10.0.18362 N/A ビルド 18362
+システムの種類:         x64-based PC
+
+- node.js  
+node-v10.16.0-win-x64
+request@2.88.0
+
+# 手順
+1. twitchにアプリ登録
+APIを使用するアプリ(≠拡張機能)を登録する。
+(要2段階認証。
+登録後、再度登録したアプリを「管理」で開き、クライアントIDが表示されていないなら「新しい秘密」で発行する。)
+https://dev.twitch.tv/console/apps
+
+1. 自分のID確認
+下記のコマンドを実行して自分のID(≠ログインID)を確認する。
+コマンド出力のid属性の値が自分のIDである。
+`curl -H "Client-ID: <クライアントID>" -X GET "https://api.twitch.tv/helix/users?login=<ログインID>"`
+
+1. node.jsにrequestモジュールをインストール
+`npm install request`
+
+1. 本ツールをダウンロード
+https://github.com/github895439/twitch_ff_check
+
+1. ダウンロードしたものを展開
+
+1. ツールの設定
+ff_check.js内の下記を設定する。
+```javascript:ff_check.js
+const CLIENT_ID = "<クライアントID>";
+const SELF_ID = "<自分のID(数値)>";
+```
+
+1. ツールを実行
+`node ff_check.js`
+
+```plaintext:標準出力
+>node ff_check.js
+https://api.twitch.tv/helix/users/follows?from_id=略&first=100
+https://api.twitch.tv/helix/users/follows?to_id=略&first=100
+following: 略
+follower: 略
+
+# both
+略
+
+# onlyToSelf
+略
+
+# onlyFromSelf
+略
+
+>
+```
+
+# 備考
+大したことではないけど。
+前はフォローもフォロワも誰もが見えてたけど、今はフォローは本人しか見えない。
+けど、APIからだと他人のフォローも見える。
+クリエイターダッシュボードと今までの画面とでUIが統一されてないから過渡だったりするせいかも。
+か、後から修正されたり。
+1アクセスでの最大取得は100で、私は100もいないのでツールは対応していない。
+対応する場合は、レスポンスのpaginationを次のクエリーにafterで指定すればできると思う。
+
+# 参考
+- twitch API
+https://dev.twitch.tv/docs/api/reference
+
+- requestモジュール
+https://github.com/request/request
---
+++ ff_check.js
@@ -0,0 +1,221 @@
+var request        = require('request');
+
+const CLIENT_ID = "<クライアントID>";
+const SELF_ID = "<自分のID>";
+var BASE_TWITCH_API_URL = "https://api.twitch.tv/helix";
+var FOLLOW_API_URL = "/users/follows";
+var DEBUG = "OFF";
+
+var listSrc;
+var listDst;
+
+function debug(str)
+{
+    if (DEBUG == "ON")
+    {
+        if (str != "func")
+        {
+            console.debug("[DEBUG] " + str);
+        }
+        else
+        {
+            func();
+        }
+    }
+}
+
+function makeQuery(params)
+{
+    let ret = "?";
+
+    for (let key of Object.keys(params))
+    {
+        ret += key + "=" + params[key] + "&";
+    }
+
+    ret = ret.substr(0, ret.length - 1);
+    return ret;
+}
+
+function syncRequest(url)
+{
+    console
+    return new Promise(
+        function(resolve, reject)
+        {
+            console.log(url);
+            request(
+                {
+                    url: url,
+                    method: "GET",
+                    headers:
+                        {
+                            "Client-ID": CLIENT_ID
+                        }
+                },
+                function(error, response, body)
+                {
+                    let ret =
+                        {
+                            resp: response,
+                            statusCode: response.statusCode,
+                            body: body
+                        };
+                    debug("func",
+                        function()
+                        {
+                            for (let key of Object.keys(response))
+                            {
+                                if (!key.startsWith("_"))
+                                {
+                                    console.log(key + ":" + response[key]);
+                                }
+                            }
+                        });
+                    debug(body);
+                    resolve(ret);
+                    return;
+                });
+        });
+}
+
+async function callFollowApi()
+{
+    let params;
+    let query;
+    let url;
+    let respApi;
+
+    params =
+        {
+            "from_id": SELF_ID,
+            "first": "100"
+        };
+
+    query = makeQuery(params);
+
+    url = BASE_TWITCH_API_URL + FOLLOW_API_URL + query;
+
+    respApi = await syncRequest(url);
+
+    if (!respApi.resp)
+    {
+        console.error("E:response none.")
+        process.exit();
+    }
+
+    if (respApi.statusCode != 200)
+    {
+        console.error("E:statuscode " + respApi.statusCode);
+        process.exit();
+    }
+
+    let jsonFrom = JSON.parse(respApi.body);
+
+    params =
+        {
+            "to_id": SELF_ID,
+            "first": "100"
+        };
+
+    query = makeQuery(params);
+
+    url = BASE_TWITCH_API_URL + FOLLOW_API_URL + query;
+
+    respApi = await syncRequest(url);
+
+    if (!respApi.resp)
+    {
+        console.error("E:response none.")
+        process.exit();
+    }
+
+    if (respApi.statusCode != 200)
+    {
+        console.error("E:statuscode " + respApi.statusCode);
+        process.exit();
+    }
+
+    let jsonTo = JSON.parse(respApi.body);
+
+    let ret =
+        {
+            from: jsonFrom,
+            to: jsonTo
+        };
+    return ret;
+}
+
+function ffCheck()
+{
+    listDst =
+        {
+            both: [],
+            onlyFromSelf: [],
+            onlyToSelf: []
+        };
+
+    for (let index = 0; index < listSrc.from.length; index++) {
+        if (listSrc.to.includes(listSrc.from[index]))
+        {
+            listDst.both.push(listSrc.from[index]);
+        }
+        else
+        {
+            listDst.onlyToSelf.push(listSrc.from[index]);
+        }
+    }
+
+    for (let index = 0; index < listSrc.to.length; index++) {
+        if (!listSrc.from.includes(listSrc.to[index]))
+        {
+            listDst.onlyFromSelf.push(listSrc.to[index]);
+        }
+    }
+}
+
+async function wrapper()
+{
+    respApi = await callFollowApi();
+
+    console.log("following: " + respApi.from.data.length);
+    console.log("follower: " + respApi.to.data.length);
+    console.log("");
+
+    if ((respApi.from.data.length >= 100) || (respApi.to.data.length >= 100))
+    {
+        console.error("E:This cannot be used with more than 100.");
+        process.exit();
+    }
+
+    listSrc =
+        {
+            from: [],
+            to: [],
+        };
+
+    for (let index = 0; index < respApi.to.data.length; index++) {
+        listSrc.from.push(respApi.to.data[index].from_name);
+    }
+
+    listSrc.from.sort();
+
+    for (let index = 0; index < respApi.from.data.length; index++) {
+        listSrc.to.push(respApi.from.data[index].to_name);
+    }
+
+    listSrc.to.sort();
+
+    ffCheck();
+
+    console.log("# both");
+    console.log(listDst.both.join("\n"));
+    console.log("");
+    console.log("# onlyToSelf");
+    console.log(listDst.onlyToSelf.join("\n"));
+    console.log("");
+    console.log("# onlyFromSelf");
+    console.log(listDst.onlyFromSelf.join("\n"));
+}
+
+wrapper();
